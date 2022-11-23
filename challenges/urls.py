e0464bbb532d137d0934af68e1925f3b9c3d1d35
beckoningstranger <jan.eisen@tutanota.de>
2022-11-24 00:06:28 +0100

    Practicing URLs, Views and Dynamic View Logic

--- challenges/urls.py
+++ challenges/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("", views.index),  # /challenges/
     # if path can be converted to an integer, call one function, if not, another
     path("<int:month>", views.monthly_challenge_by_number),
     path("<str:month>", views.monthly_challenge, name="month-challenge")
+
 ]
--- challenges/views.py
+++ challenges/views.py
@@ -21,19 +21,38 @@ monthly_challenges = {
 # Create your views here.
 
 
+def index(request):
+    list_items = ""
+    months = list(monthly_challenges.keys())
+
+    for month in months:
+        month_path = reverse("month-challenge", args=[month])
+        list_items += f"<li><a href='{month_path}'>{month.capitalize()}</a></li>"
+
+    response_data = f"""
+        <ul>
+            {list_items}
+        </ul>
+    """
+    return HttpResponse(response_data)
+
+
 def monthly_challenge_by_number(request, month):
     months = list(monthly_challenges.keys())
-    
+
     if month >= len(months):
         return HttpResponseNotFound("Invalid month")
 
     redirect_month = months[month - 1]
-    redirect_path = reverse("month-challenge", args=[redirect_month]) # /challenge/january
+    redirect_path = reverse(
+        "month-challenge", args=[redirect_month])  # /challenge/january
     return HttpResponseRedirect(redirect_path)
 
 
 def monthly_challenge(request, month):
     try:
-        return HttpResponse(monthly_challenges[month])
+        challenge_text = monthly_challenges[month]
+        response_data = f"<h1>{challenge_text}</h1>"
+        return HttpResponse(response_data)
     except KeyError:
-        return HttpResponseNotFound("Month not implemented yet!")
+        return HttpResponseNotFound("<h1>Month not implemented yet!</h1>")

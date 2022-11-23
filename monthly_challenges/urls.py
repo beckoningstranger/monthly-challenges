2c7907ec20163aca65e175a3e03634e5c02c4912
beckoningstranger <jan.eisen@tutanota.de>
2022-11-23 16:20:29 +0100

    Creating a First View & URL

--- monthly_challenges/urls.py
+++ monthly_challenges/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # This points to app "challenges" and file "urls" in that app, note the trailing /
+    path('challenges/', include("challenges.urls")) 
 ]

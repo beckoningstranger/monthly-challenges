2c6fc80309a509d34dc17083eaa02f60a936c306
beckoningstranger <jan.eisen@tutanota.de>
2022-11-24 12:11:43 +0100

    Including Partial Template Snippets

--- challenges/urls.py
+++ challenges/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index),  # /challenges/
+    path("", views.index, name="index"),  # /challenges/
     # if path can be converted to an integer, call one function, if not, another
     path("<int:month>", views.monthly_challenge_by_number),
     path("<str:month>", views.monthly_challenge, name="month-challenge")
